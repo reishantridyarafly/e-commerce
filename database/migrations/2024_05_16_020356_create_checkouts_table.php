ddef06e61cfd7b5243ffb78514b74a292049b247
Reishan10 <reishantridyarafly@gmail.com>
2024-05-16 02:12:14 +0700

    Feat Checkout: create checkout migration

---
+++ database/migrations/2024_05_16_020356_create_checkouts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('checkouts', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('user_id')->nullable();
+            $table->uuid('address_id')->nullable();
+            $table->decimal('total_biaya', 15, 2);
+            $table->string('status')->default('pending')->comment('pending, completed, failed');
+            $table->string('bukti_pembayaran')->nullable();
+            $table->timestamp('tanggal_pembayaran');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+            $table->foreign('address_id')->references('id')->on('address')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('checkouts');
+    }
+};
