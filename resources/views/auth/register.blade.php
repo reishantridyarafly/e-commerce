ef061f9642425033366b52f4a85a2e8785d0013f
reishantridyarafly <reishantridyarafly@gmail.com>
2024-09-23 20:50:57 +0700

    Fix register: Fixing bug in register feature

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -205,7 +205,7 @@
                                 text: 'Data berhasil disimpan',
                             }).then(function() {
                                 top.location.href =
-                                    "{{ route('login.index') }}";
+                                    "{{ route('login') }}";
                             });
                         }
                     },
